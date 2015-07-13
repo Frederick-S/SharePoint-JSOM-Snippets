d86548ab42648ee94c866abb53a7ef577179f64c
Frederick-S <mao_xiaodan@hotmail.com>
2015-07-13 16:00:04 +0800

    Init

--- config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var config = {
-    db: 'mongodb://localhost/memory',
-    sessionSecret: 'memory session secret'
-};
-
-module.exports = config;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var index = {};
-
-index.get = function (req, res) {
-    res.render('index', { title: 'Memory', user: req.session.user });
-};
-
-exports.index = index;
